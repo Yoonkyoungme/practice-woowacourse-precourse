a8ba4bab60249d55bce952a549eb3b29870c4791
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-12-12 23:42:16 +0900

    feat: 숫자 입력 받기

--- javascript-baseball/src/Game.js
+++ javascript-baseball/src/Game.js
@@ -1,22 +1,31 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants/messages");
+const Validation = require("./Validation");
 
 class Game {
   constructor() {
     this.computerNumbers = [];
+    this.numbersList = "";
     this.start();
   }
   start() {
-    this.selectComputerNumber();
-    console.log(this.computerNumbers);
+    this.selectComputerNumbers();
+    this.selectNumbers();
   }
 
-  selectComputerNumber() {
+  selectComputerNumbers() {
     while (this.computerNumbers.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
       if (!this.computerNumbers.includes(number)) {
         this.computerNumbers.push(number);
       }
     }
+  }
+
+  selectNumbers() {
+    Console.readLine(MESSAGE.INPUT, (numbers) => {
+      console.log(numbers);
+    });
   }
 }
 

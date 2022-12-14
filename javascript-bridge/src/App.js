c06ce2307e70fcaad7c7777226bfbfef051fd2c8
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-12-14 22:25:53 +0900

    feat: 게임 시작 문구 출력

--- javascript-bridge/src/App.js
+++ javascript-bridge/src/App.js
@@ -1,5 +1,14 @@
+const BridgeGame = require("./BridgeGame");
+const { INPUT } = require("./constants/messages");
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print(INPUT.START);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ javascript-bridge/src/constants/messages.js
@@ -0,0 +1,5 @@
+const INPUT = {
+  START: "다리 건너기 게임을 시작합니다.",
+};
+
+module.exports = { INPUT };

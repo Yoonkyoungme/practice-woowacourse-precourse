a8e8bbbf3b26199ac2980763575a8d8c5950d486
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-12-15 01:31:12 +0900

    feat: 이동할 칸 입력 받기

--- javascript-bridge/src/Game.js
+++ javascript-bridge/src/Game.js
@@ -11,9 +11,19 @@ class Game {
     InputView.readBridgeSize(this.handleBridgeSize.bind(this));
   }
 
+  inputMovingBridge() {
+    InputView.readMoving(this.handleMovingBridge.bind(this));
+  }
+
   handleBridgeSize(size) {
     const getValue = Validation.checkInputNumber(size);
     this.bridgeSize = getValue;
+
+    this.inputMovingBridge();
+  }
+
+  handleMovingBridge(moving) {
+    console.log(moving);
   }
 }
 
--- javascript-bridge/src/constants/messages.js
+++ javascript-bridge/src/constants/messages.js
@@ -1,6 +1,7 @@
 const INPUT = {
   START: "다리 건너기 게임을 시작합니다.\n",
   SIZE: "다리의 길이를 입력해주세요.\n",
+  MOVING: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
 };
 
 const ERROR = {

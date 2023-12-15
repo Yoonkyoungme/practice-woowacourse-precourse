a49662731e4f39395a46670a9590938dbf0de251
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2023-12-15 23:18:32 +0900

    feat: 메시지 상수 파일 추가

---
+++ javascript-racingcar-6/src/utils/constants/messages.js
@@ -0,0 +1,15 @@
+export const INPUT_MESSAGES = {
+  CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  RACE_TIMES: '시도할 횟수는 몇 회인가요?',
+};
+
+export const OUTPUT_MESSAGES = {
+  RESULT_TITLE: '실행 결과\n',
+  WINNER_ANNOUNCEMENT: '최종 우승자 : ',
+};
+
+export const ERROR_MESSAGES = {
+  INVALID_CAR_NAME:
+    '[ERROR] 유효하지 않은 자동차 이름입니다. 다시 입력해 주세요.',
+  INVALID_RACE_TIMES: '[ERROR] 유효하지 않은 횟수입니다. 다시 입력해 주세요.',
+};
